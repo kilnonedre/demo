346808dc2de263bdcca0699bd00298e2689d7803
kilnonedre <1600513615@qq.com>
2024-05-16 11:55:39 +0800

    build: 111

--- .cz-config.cjs
+++ .cz-config.cjs
@@ -1,52 +1,52 @@
 module.exports = {
   types: [
     {
-      value: '✨ feat: ',
-      name: '✨ feat:     新功能',
+      value: 'feat',
+      name: '✨ feat      新功能',
     },
     {
-      value: '🐛 fix:',
-      name: '🐛 fix:      修复bug',
+      value: 'fix',
+      name: '🐛 fix       修复bug',
     },
     {
-      value: '📦️ build:',
-      name: '📦️ build:    打包',
+      value: 'build',
+      name: '📦️ build     打包',
     },
     {
-      value: '⚡️ perf:',
-      name: '⚡️ perf:     性能优化',
+      value: 'perf',
+      name: '⚡️ perf      性能优化',
     },
     {
-      value: '🎉 release:',
-      name: '🎉 release:  发布正式版',
+      value: 'release',
+      name: '🎉 release   发布正式版',
     },
     {
-      value: '💄 style:',
-      name: '💄 style:    代码的样式美化',
+      value: 'style',
+      name: '💄 style     代码的样式美化',
     },
     {
-      value: '♻️  refactor:',
-      name: '♻️  refactor: 重构',
+      value: 'refactor',
+      name: '♻️  refactor  重构',
     },
     {
-      value: '✏️  docs:',
-      name: '✏️  docs:     文档变更',
+      value: 'docs',
+      name: '📝 docs      文档变更',
     },
     {
-      value: '✅ test:',
-      name: '✅ test:     测试',
+      value: 'test',
+      name: '✅ test      测试',
     },
     {
-      value: '⏪️ revert:',
-      name: '⏪️ revert:   回退',
+      value: 'revert',
+      name: '⏪️ revert    回退',
     },
     {
-      value: '🚀 chore:',
-      name: '🚀 chore:    构建/工程依赖/工具',
+      value: 'chore',
+      name: '🚀 chore     构建/工程依赖/工具',
     },
     {
-      value: '👷 ci:',
-      name: '👷 ci:       CI related changes',
+      value: 'ci',
+      name: '👷 ci        CI 相关变更',
     },
   ],
   messages: {
@@ -58,9 +58,8 @@ module.exports = {
     footer: '请输入要关闭的issue(可选)',
     confirmCommit: '确定提交此说明吗？',
   },
-  allowCustomScopes: false,
+  allowCustomScopes: true,
   // 跳过问题
-  skipQuestions: ['body', 'footer'],
+  skipQuestions: ['body', 'footer', 'scope'],
   subjectLimit: 72,
-  // scopes: [],
 }
